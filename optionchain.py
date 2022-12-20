7ee430fc276d968d91d31bdeb7b075084aa1ecb8
ritwikthakar <80002638+ritwikthakar@users.noreply.github.com>
2022-12-20 12:34:25 -0500

    Update optionchain.py

--- optionchain.py
+++ optionchain.py
@@ -103,6 +103,7 @@ def rho(r,s,k,T,sigma,type = 'c'):
 
 import pandas as pd
 import yfinance as yf
+from yahoo_fin import options as op
 import datetime
 import matplotlib.pyplot as plt
 
@@ -115,7 +116,7 @@ symbol = st.sidebar.text_input('Enter Ticker', 'SPY')
 tk = yf.Ticker(symbol)
 r = st.sidebar.number_input('Enter Risk Free Rate', 1, value=(5))
 r = r/100
-expiry = st.sidebar.date_input("Start Date",datetime.date(2024, 1, 19))
+expiry = st.sidebar.date_input("Start Date",datetime.date(2023, 1, 20))
 expiry = expiry.strftime('%Y-%m-%d')
 close_data = tk.history(period='1d')
 close = close_data.Close.values
@@ -296,7 +297,80 @@ plt.title(f'{symbol.upper()} Option Volume')
 # st.subheader('Option Volume')
 # st.pyplot(fig2)
 
-tab1, tab2, tab3, tab4, tab5 = st.tabs(['Option Chain', 'Individual Call Strike Analyzer','Max Pain' , "Open Interest", "Option Volume"])
+def getVerticalSpreadPrice(ticker, spreadType, expNo, longStrike, shortStrike):
+    
+# #inputs
+# ticker = 'PLTR'
+# spreadType = 'put' #call or put
+# expNo = 5
+# longStrike = 20
+# shortStrike = 19
+
+    try:
+    
+        #get expiration dates
+        expirationDates = op.get_expiration_dates(ticker)
+        
+        st.write('- - - Getting Data - - -\n')
+        
+        #get data based on spread type, call or put spread
+        if spreadType == 'call':
+            chainData = op.get_calls(ticker, date = expirationDates[expNo])
+        elif spreadType == 'put':
+            chainData = op.get_puts(ticker, date = expirationDates[expNo])
+        else: 
+            st.write('Please enter call or put for spreadType.')
+            return
+        
+        #trim data 
+        chainData = chainData[['Strike', 'Bid', 'Ask', 'Last Price']][
+            (chainData['Strike'] == longStrike) |
+            (chainData['Strike'] == shortStrike)]
+        
+        #reset index
+        chainData = chainData.reset_index(drop = True)
+        
+        #change to numeric data type
+        chainData['Strike'] = pd.to_numeric(
+                chainData['Strike'], errors = 'coerce')
+        chainData['Bid'] = pd.to_numeric(
+                chainData['Bid'], errors = 'coerce')
+        chainData['Ask'] = pd.to_numeric(
+                chainData['Ask'], errors = 'coerce')
+        chainData['Last Price'] = pd.to_numeric(
+                chainData['Last Price'], errors = 'coerce')
+        
+        #create mid price for reference
+        chainData['Mid'] = (chainData['Bid'] + chainData['Ask']) / 2
+        
+        st.write(chainData)
+        st.write('')
+        
+        #get spread price
+        spreadPrice = chainData['Ask'][chainData['Strike'] == longStrike].iloc[0
+                  ] - chainData['Bid'][chainData['Strike'] == shortStrike].iloc[0]
+        
+        #print pricing
+        if spreadType == 'call' and longStrike < shortStrike:
+            st.write('Long Call Spread Price is $' + str(round(spreadPrice, 4)))
+        elif spreadType == 'call' and longStrike > shortStrike:
+            st.write('Short Call Spread Price is -$' + str(abs(round(spreadPrice, 4))))
+        elif spreadType == 'put' and longStrike > shortStrike:
+            st.write('Long Put Spread Price is $' + str(round(spreadPrice, 4)))
+        elif spreadType == 'put' and longStrike < shortStrike:
+            st.write('Short Put Spread Price is -$' + str(abs(round(spreadPrice, 4))))
+        elif longStrike == shortStrike:
+            st.write('Strike prices are the same, no vertical spreads here.')
+            return
+            
+        #output
+        return  round(spreadPrice, 4)
+    
+    except IndexError:
+        st.write('Strike data not available, try again.')
+        
+        
+tab1, tab2, tab3, tab4, tab5, tab6 = st.tabs(['Option Chain', 'Individual Strike','Max Pain' , "Open Interest", "Option Volume", "Vertical Spreads"])
 
 with tab1:
     st.header("Option Chain")
@@ -325,3 +399,14 @@ with tab4:
 with tab5:
     st.header("Option Volume")
     st.pyplot(fig2)
+
+with tab6:
+    strike1 = st.selectbox('Select Long Strike:', call_strike)
+    strike2 = st.selectbox('Select Short Strike:', call_strike)
+    option_type = st.selectbox('Select Call or Put', ('call', 'put'))
+    expirationDates = op.get_expiration_dates(symbol)
+    select_expiry = st.selectbox('Select Expiry', (1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26))
+    getVerticalSpreadPrice(symbol, option_type, select_expiry, strike1, strike2)
+    for i,each in enumerate(expirationDates,start=1):
+        st.write("{}.{}".format(i,each))
+    
