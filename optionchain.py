827d1447960eb3446b3da4aad2441f63b111f456
ritwikthakar <80002638+ritwikthakar@users.noreply.github.com>
2023-05-10 22:45:33 -0400

    Update optionchain.py

--- optionchain.py
+++ optionchain.py
@@ -318,7 +318,7 @@ atmCall = opt.calls.loc[opt.calls['inTheMoney'] == False].iloc[0]
 atmPuts = opt.puts.loc[opt.puts['inTheMoney'] == False].iloc[0]
 
 move = (atmCall['lastPrice'] +
-        atmPuts['lastPrice']) * 0.98
+        atmPuts['lastPrice'])
 
 upper_move = close + move
 lower_move = close - move
