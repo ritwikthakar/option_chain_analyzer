278627fb5c3eb5356dc40f39ab26bdd031059b5e
ritwikthakar <80002638+ritwikthakar@users.noreply.github.com>
2023-05-30 22:57:18 -0400

    Update optionchain.py

--- optionchain.py
+++ optionchain.py
@@ -210,81 +210,84 @@ def options_chain(tk, expiry):
     '''
     # Get options exp
     options = pd.DataFrame()
-    opt = tk.option_chain(expiry)
-    call_opts = opt.calls
-    put_opts = opt.puts
-    call_opts['CALL'] = 'c'
-    put_opts['CALL'] = 'p'
-    call_opts['expirationDate'] = expiry
-    put_opts['expirationDate'] = expiry
+    opt = tk.option_chain(expiry.strip())
+    opt = pd.concat([opt.calls, opt.puts], ignore_index=True)SP
+    opt['expirationDate'] = expiry
+    options = pd.concat([options, opt], ignore_index=True)
+
     # Add 1 day to get the correct expiration date
-    call_opts['expirationDate'] = pd.to_datetime(call_opts['expirationDate']) + datetime.timedelta(days = 1)
-    call_opts['dte'] = (call_opts['expirationDate'] - datetime.datetime.today()).dt.days / 365
-    # Add 1 day to get the correct expiration date
-    put_opts['expirationDate'] = pd.to_datetime(put_opts['expirationDate']) + datetime.timedelta(days = 1)
-    put_opts['dte'] = (put_opts['expirationDate'] - datetime.datetime.today()).dt.days / 365
+    options['expirationDate'] = pd.to_datetime(options['expirationDate']) + datetime.timedelta(days=1)
+    options['dte'] = (options['expirationDate'] - datetime.datetime.today()).dt.days / 365
+
+    # Boolean column if the option is a CALL
+    options['CALL'] = options['contractSymbol'].str[4:].apply(
+        lambda x: "C" in x)
+
+    options[['bid', 'ask', 'strike']] = options[['bid', 'ask', 'strike']].apply(pd.to_numeric)
+
     # Drop unnecessary and meaningless columns
-    call_opts = call_opts.drop(columns = ['contractSize', 'currency', 'change', 'percentChange', 'lastTradeDate'])
-    put_opts = put_opts.drop(columns = ['contractSize', 'currency', 'change', 'percentChange', 'lastTradeDate'])
-    options = pd.concat([call_opts,put_opts])
-    
+    options = options.drop(
+        columns=['contractSize', 'currency', 'change', 'percentChange', 'lastTradeDate', 'lastPrice', 'contractSymbol',
+                 'bid', 'ask', 'impliedVolatility', 'inTheMoney', 'dte'])
+
     return options
+
 
 chain = options_chain(tk, expiry)
 
 def total_loss_on_strike(chain, expiry_price):
     '''
     Get's the total loss at the given strike price
-    '''    
+    '''
     # call options with strike price below the expiry price -> loss for option writers
     callChain = chain.loc[chain['CALL'] == True]
-    callChain = callChain.dropna()       
+    callChain = callChain.dropna()
     in_money_calls = callChain[callChain['strike'] < expiry_price][["openInterest", "strike"]]
-    in_money_calls["CLoss"] = (expiry_price - in_money_calls['strike'])*in_money_calls["openInterest"]
+    in_money_calls["CLoss"] = (expiry_price - in_money_calls['strike']) * in_money_calls["openInterest"]
 
     # get puts n drop null values
     putChain = chain.loc[chain['CALL'] == False]
-    putChain = putChain.dropna()    
-    
+    putChain = putChain.dropna()
+
     # put options with strike price above the expiry price -> loss for option writers
     in_money_puts = putChain[putChain['strike'] > expiry_price][["openInterest", "strike"]]
-    in_money_puts["PLoss"] = (in_money_puts['strike'] - expiry_price)*in_money_puts["openInterest"]
+    in_money_puts["PLoss"] = (in_money_puts['strike'] - expiry_price) * in_money_puts["openInterest"]
     total_loss = in_money_calls["CLoss"].sum() + in_money_puts["PLoss"].sum()
 
     return total_loss
 
 strikes = chain.get(['strike']).values.tolist()
-losses = [total_loss_on_strike(chain, strike[0]) for strike in strikes] 
+losses = [total_loss_on_strike(chain, strike[0]) for strike in strikes]
 
 # max pain min loss to option writers/sellers at strike price
 flat_strikes = [item for sublist in strikes for item in sublist]
 point = losses.index(min(losses))
 max_pain = flat_strikes[point]
 buffer = 3
-bufferHigh = max_pain + (max_pain * (buffer/100))
-bufferLow = max_pain - (max_pain * (buffer/100))
-
+bufferHigh = max_pain + (max_pain * (buffer / 100))
+bufferLow = max_pain - (max_pain * (buffer / 100))
+print(f"Maximum Pain: {bufferLow} < {max_pain} < {bufferHigh}")
 
 # calc put to call ratio
 callChain = chain.loc[chain['CALL'] == True]
 putChain = chain.loc[chain['CALL'] == False]
 pcr = putChain["volume"].sum() / callChain["volume"].sum()
-
+print("Put to call ratio:", round(pcr, 2))
 
 # get the cummulated losses
 total = {}
 for i in range(len(flat_strikes)):
-    if flat_strikes[i] not in total: total[flat_strikes[i]] = losses[i]
-    else: total[flat_strikes[i]] += losses[i]
-
+    if flat_strikes[i] not in total:
+        total[flat_strikes[i]] = losses[i]
+    else:
+        total[flat_strikes[i]] += losses[i]
 
 # plot
 keys = set(list(total.keys()))
-fig = plt.figure(figsize = (15, 6))
 plt.bar(list(keys), list(total.values()), width=1)
 plt.xlabel('Strike Price')
-plt.ylabel('Maximum Loss')
 plt.title(f'{symbol.upper()} Max Pain')
+
 
 fig1 = plt.figure(figsize = (15, 6))
 plt.bar(call_df['strike'],call_df['openInterest'], label="Calls")
